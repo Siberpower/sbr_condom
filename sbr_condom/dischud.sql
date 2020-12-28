d91d506418c88f782115321612d5a554383375e9
Siberpower <76598676+Siberpower@users.noreply.github.com>
2020-12-28 17:20:07 +0300

    Delete dischud.sql

--- sbr_condom/dischud.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
-    ('condom3', 'XL Kondom', 0.5, 0, 1);
-    ('condom2', 'L Kondom', 0.5, 0, 1);
-    ('condom1', 'S Kondom', 0.4, 0, 1);
-    ('condom', 'XS Kondom', 0.4, 0, 1);
