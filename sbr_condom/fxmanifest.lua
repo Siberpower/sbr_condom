2d5d6df83946a78e56c0ebe62a51907e9bcb3d03
Siberpower <76598676+Siberpower@users.noreply.github.com>
2021-01-01 21:45:41 +0300

    Update fxmanifest.lua

--- sbr_condom/fxmanifest.lua
+++ sbr_condom/fxmanifest.lua
@@ -4,12 +4,8 @@ game "gta5"
 
 client_scripts {
     'client/client.lua',
-    '@es_extended/locale.lua',
-    'config.lua'
 }
 
 server_scripts {
-    '@es_extended/locale.lua',
-    'server/server.lua',
-    'config.lua'
+    'server/server.lua'
 }
